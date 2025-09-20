d59c1c0b60417f69b6d699f4a246b59b0daaef5d
Irsam01 <irsamsabir1@gmail.com>
2025-09-21 03:51:41 +0500

    chore: move project files into backend folder

--- core/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-class EmployeeReqSerializer(serializers.Serializer):
-    id = serializers.CharField()
-    access_level = serializers.IntegerField()
-    request_time = serializers.TimeField(format="%H:%M", input_formats=["%H:%M"])
-    room = serializers.CharField()
