6b4762abae841167c5fe1cfe2504ad90276a7783
Irsam01 <irsamsabir1@gmail.com>
2025-09-21 01:15:39 +0500

    create simulate access api

--- accesssim/urls.py
+++ accesssim/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import SimulateAccessView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/simulate/", SimulateAccessView.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from core.serializers import EmployeeReqSerializer
+from core.mainlogic import simulate_batch
 
-# Create your views here.
+class SimulateAccessView(APIView):
+    def post(self, request):
+        serializer = EmployeeReqSerializer(data=request.data.get("employees", []), many=True)
+        serializer.is_valid(raise_exception=True)
+        results = simulate_batch(serializer.validated_data)
+        return Response(results)
+
+
